bbe8a1f3acaed929aafadf1d3c0e15100155390b
mehere10 <nishantraturi10@gmail.com>
2024-08-07 13:09:28 +0530

    added Basics2.java

--- LinkedListDataStructure/Basics2.java
+++ LinkedListDataStructure/Basics2.java
@@ -1,2 +1,47 @@
-package LinkedListDataStructure;public class Basics2 {
+package LinkedListDataStructure;
+
+public class Basics2
+{
+
+    public static void display(Node head)
+    {
+        Node temp=head;
+
+        while(head!=null)
+        {
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+    }
+    static class Node
+    {
+        int data;
+        Node next;      // this is the address of the next node.
+        public Node(int data)       // constructor
+        {
+            this.data=data;
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        Node a=new Node(5);
+        Node b=new Node(6);
+        Node c=new Node(7);
+        Node d=new Node(8);
+        Node e=new Node(100);
+
+        // connect the elements with each other
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+
+
+        // printing elements of linked list using loop.
+
+       display(a);
+
+    }
 }
