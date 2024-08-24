33a8afc6bbd6bb16bf7a563512d5269342052119
mehere10 <nishantraturi10@gmail.com>
2024-08-24 21:11:03 +0530

    added a temp file

---
+++ StringDataStructure/Temp.java
@@ -0,0 +1,15 @@
+package StringDataStructure;
+import java.util.*;
+public class Temp
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String s="Nishant";
+
+        char result=s.charAt(0);
+        int length=s.length();
+        System.out.println(result);
+        System.out.println(length);
+    }
+}
