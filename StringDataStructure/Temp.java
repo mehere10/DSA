9ad0bbfa43a88b07c9afc3399ee5326eaa73c83d
mehere10 <nishantraturi10@gmail.com>
2024-08-25 14:14:32 +0530

    updated a file

--- StringDataStructure/Temp.java
+++ StringDataStructure/Temp.java
@@ -2,14 +2,22 @@ package StringDataStructure;
 import java.util.*;
 public class Temp
 {
+        static int isVowel(char ch)
+        {
+            return (0x208222 >> (ch & 0x1f)) & 1;
+            // same as (2130466 >> (ch & 31)) & 1;
+        }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+//        Scanner sc=new Scanner(System.in);
+//
+//        String s="Nishant";
+//
+//        char result=s.charAt(0);
+//        int length=s.length();
+//        System.out.println(result);
+//        System.out.println(length);
+        System.out.println("a is " + isVowel('a'));
+        System.out.println("x is " + isVowel('x'));
 
-        String s="Nishant";
-
-        char result=s.charAt(0);
-        int length=s.length();
-        System.out.println(result);
-        System.out.println(length);
     }
 }
