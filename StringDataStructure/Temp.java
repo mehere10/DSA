19cb68ab12d64fc9aebab1a8f6ed4590c1aaa0b5
mehere10 <nishantraturi10@gmail.com>
2024-08-26 14:09:31 +0530

    added new file

--- StringDataStructure/Temp.java
+++ StringDataStructure/Temp.java
@@ -2,29 +2,5 @@ package StringDataStructure;
 import java.util.*;
 public class Temp
 {
-    static String myMethod(String str)
-    {
-        String result="";
-        for(int i=0;i<str.length();i++)
-        {
-            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i'
-                    || str.charAt(i)=='o' || str.charAt(i)=='u' )
-            {
-                result=result+"v";
-            }
-            else
-            {
-                result=result+"c";
-            }
-        }
-        return result;
-    }
-    public static void main(String[] args) {
 
-        String str="hello";     // cvccv
-
-        System.out.println(myMethod(str));
-
-
-    }
 }
