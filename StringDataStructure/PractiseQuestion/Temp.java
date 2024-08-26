72fbe97c9a09622306f08137b85e9869f51c62ad
mehere10 <nishantraturi10@gmail.com>
2024-08-26 14:10:11 +0530

    added new file

--- StringDataStructure/PractiseQuestion/Temp.java
+++ StringDataStructure/PractiseQuestion/Temp.java
@@ -1,4 +1,30 @@
 package StringDataStructure.PractiseQuestion;
 
-public class Temp {
+public class Temp
+{
+    static String myMethod(String str)
+    {
+        String result="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i'
+                    || str.charAt(i)=='o' || str.charAt(i)=='u' )
+            {
+                result=result+"v";
+            }
+            else
+            {
+                result=result+"c";
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+        String str="hello";     // cvccv
+
+        System.out.println(myMethod(str));
+
+
+    }
 }
