b97c41f6ffef9aa7a23545b8c46d14e5da5d7d02
mehere10 <nishantraturi10@gmail.com>
2024-08-28 20:21:15 +0530

    updated a file

--- StringDataStructure/PractiseQuestion/QuestionOne.java
+++ StringDataStructure/PractiseQuestion/QuestionOne.java
@@ -5,7 +5,7 @@ public class QuestionOne
     static String myMethod(String str)
     {
         String result="";
-        for(int i=0;i<str.length();i++)
+        for(int i=0;i<str.length(); i++)
         {
             if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i'
                     || str.charAt(i)=='o' || str.charAt(i)=='u' )
