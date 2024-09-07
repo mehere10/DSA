da07f709d269ade3edb4109fa6280a91aeb5e494
mehere10 <nishantraturi10@gmail.com>
2024-09-07 11:32:13 +0530

    updated

--- StringDataStructure/StringBulder/stringBulderMethods.java
+++ StringDataStructure/StringBulder/stringBulderMethods.java
@@ -10,12 +10,24 @@ public class stringBulderMethods
     static String strAppend(String str)
     {
         StringBuilder result=new StringBuilder(str);
-        result.append(" XYZ");  // append method
+        result.append(" World");  // append method
+
+        // we are using toString method because SB returns an object of SB , to we need to convert it into String.
         return result.toString();
     }
+
+    // setCharAt method
+    static String setCharacter(String str)
+    {
+        StringBuilder result=new StringBuilder(str);
+        result.setCharAt(0,'M');
+        return result.toString();
+    }
+    static String
     public static void main(String[] args) {
 
-        String s="ABC";
-        System.out.println(strAppend(s));
+        String s="Hello";
+        System.out.println(strAppend(s));   // append
+        System.out.println(setCharacter(s));   // setCharAt
     }
 }
