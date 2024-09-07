2e0df46923377dc318a81c481fe861a04e1c2070
mehere10 <nishantraturi10@gmail.com>
2024-09-07 11:26:14 +0530

    updated

--- StringDataStructure/StringBulder/stringBulderMethods.java
+++ StringDataStructure/StringBulder/stringBulderMethods.java
@@ -1,4 +1,21 @@
 package StringDataStructure.StringBulder;
+import java.util.*;
 
-public class stringBulderMethods {
+
+public class stringBulderMethods
+{
+    Scanner sc=new Scanner(System.in);
+
+    // Append method
+    static String strAppend(String str)
+    {
+        StringBuilder result=new StringBuilder(str);
+        result.append(" XYZ");  // append method
+        return result.toString();
+    }
+    public static void main(String[] args) {
+
+        String s="ABC";
+        System.out.println(strAppend(s));
+    }
 }
