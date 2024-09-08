2f8575008a3a735b3b1b2d7e0f2ed6ccde8f5118
mehere10 <nishantraturi10@gmail.com>
2024-09-08 20:40:00 +0530

    updated

--- StringDataStructure/PractiseQuestion/upperCaseToLower.java
+++ StringDataStructure/PractiseQuestion/upperCaseToLower.java
@@ -4,13 +4,35 @@ import java.util.Scanner;
 
 public class upperCaseToLower
 {
-    public static void main(String[] args) {
+    static String caseConversion()
+    {
         Scanner sc=new Scanner(System.in);
-        StringBuilder str=new StringBuilder(sc.nextLine());
 
-        // toggle
+        // taking a user input
+        StringBuilder str=new StringBuilder(sc.nextLine());
         for(int i=0;i<str.length();i++){
-            boolean flag=true;
+            char ch=str.charAt(i);	// will get the current character.
+
+            boolean flag=true;	// it means we consider the character to be capital by default
+            int ascii=(int)ch;	// this will typecast the char to its ascii value.
+            if(ascii>=97)	// small
+            {
+                ascii-=32;
+                char temp=(char)ascii;
+                str.setCharAt(i,temp);
+            }
+            else	// capital
+            {
+                ascii+=32;
+                char temp=(char)ascii;
+                str.setCharAt(i,temp);
+            }
         }
+        return str.toString();
+    }
+    public static void main(String[] args) {
+
+        String result=caseConversion();
+        System.out.println(result);
     }
 }
