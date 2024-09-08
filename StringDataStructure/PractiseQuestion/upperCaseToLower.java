d7c9fc0e00d75ca19b8b6c951ce22903bbba8f2b
mehere10 <nishantraturi10@gmail.com>
2024-09-08 19:12:06 +0530

    updated

--- StringDataStructure/PractiseQuestion/upperCaseToLower.java
+++ StringDataStructure/PractiseQuestion/upperCaseToLower.java
@@ -1,4 +1,16 @@
 package StringDataStructure.PractiseQuestion;
 
-public class upperCaseToLower {
+import java.util.Scanner;
+
+public class upperCaseToLower
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringBuilder str=new StringBuilder(sc.nextLine());
+
+        // toggle
+        for(int i=0;i<str.length();i++){
+            boolean flag=true;
+        }
+    }
 }
