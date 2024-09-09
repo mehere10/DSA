b78bc71515c3f7d6eb89297060693933b0ccc480
mehere10 <nishantraturi10@gmail.com>
2024-09-09 21:01:28 +0530

    updated

---
+++ StringDataStructure/PractiseQuestion/numberOfPaindromeSubstr.java
@@ -0,0 +1,29 @@
+package StringDataStructure.PractiseQuestion;
+
+public class numberOfPaindromeSubstr {
+    static boolean isPalindrome(String s){
+        int i=0,j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str="abcba";
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                 if(isPalindrome(str.substring(i,j))==true) {
+                     System.out.print(str.substring(i, j) + " ");
+                     count++;
+                 }
+            }
+        }
+        System.out.println("\nNumber of palindrome substrings : "+count);
+    }
+}
