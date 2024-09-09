875d087ade54e8ccbde86438d89a25f1431bec24
mehere10 <nishantraturi10@gmail.com>
2024-09-09 21:48:54 +0530

    updated

---
+++ StringDataStructure/PractiseQuestion/reverseASentence.java
@@ -0,0 +1,29 @@
+package StringDataStructure.PractiseQuestion;
+
+public class reverseASentence {
+    static String reverse(String str){
+
+        // create an empty answer string
+        String ans="";
+
+        // create an empty stringBuilder String
+        StringBuilder sb=new StringBuilder("");
+
+        // traversing through original string
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);      // current character
+            if(ch!=' '){
+                sb.append(ch);
+            }
+            else{
+                sb.reverse();
+                ans+=sb;
+                ans+=" ";
+                sb=new StringBuilder(" ");    // deleting the entire string builder
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String str="I am an online educator";
+    }
+}
