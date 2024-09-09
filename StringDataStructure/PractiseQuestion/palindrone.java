96c62098d080db613981f3d378c397ade48519c9
mehere10 <nishantraturi10@gmail.com>
2024-09-09 20:05:11 +0530

    added new file

---
+++ StringDataStructure/PractiseQuestion/palindrone.java
@@ -0,0 +1,32 @@
+package StringDataStructure.PractiseQuestion;
+import java.util.Scanner;
+
+public class palindrone {
+
+    static void isPalindrome(String str)
+    {
+        // abcdcba
+        int i=0,j=str.length()-1;
+
+        boolean flag=true; // we created a checkpoint , it means by default a string is considered palindrome.
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                flag=false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(flag) System.out.println("The string is palindrome");
+        else System.out.println("The String is not Palindrome");
+
+    }
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String str=sc.nextLine();
+        isPalindrome(str);
+
+    }
+}
