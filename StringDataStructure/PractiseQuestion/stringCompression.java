e97c3e1be57a4844eea8cd0769f8623ab1204061
mehere10 <nishantraturi10@gmail.com>
2024-09-18 20:41:36 +0530

    updated

---
+++ StringDataStructure/PractiseQuestion/stringCompression.java
@@ -0,0 +1,31 @@
+package StringDataStructure.PractiseQuestion;
+
+public class stringCompression {
+
+    static String strCompression(String str){
+        StringBuilder ans=new StringBuilder(""+str.charAt(0));
+        int count=1;
+        for(int i=1;i<str.length();i++){
+            char curr=str.charAt(i);
+            char prev=str.charAt(i-1);
+
+            if(curr==prev){
+                count++;
+            }
+            else{
+                if(count>1)ans.append(count);
+                count=1;
+                ans.append(curr);
+            }
+        }
+        if(count>1)ans.append(count);
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+
+        String str="aabbbbbcdde";
+        String result=strCompression(str);
+
+        System.out.println(result);
+    }
+}
