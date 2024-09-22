dd8a0d5c5dd45d1a485e166d360bda880d8764aa
mehere10 <nishantraturi10@gmail.com>
2024-09-22 21:42:33 +0530

    updated

--- HashSet/Basics.java
+++ HashSet/Basics.java
@@ -1,5 +1,6 @@
 package HashSet;
 import java.util.HashSet;
+import java.util.Iterator;
 public class Basics {
     public static void main(String[] args) {
         HashSet<Integer> set=new HashSet<>();
@@ -7,11 +8,29 @@ public class Basics {
         // insert
         set.add(1);
         set.add(2);
-        set.add(1);
+        set.add(3);
 
         // search
         if(set.contains(1)) System.out.println("set contains 1");
         if(!set.contains(6)) System.out.println("Does not contain 6");
 
+        // remove an element in the set
+        set.remove(1);
+
+        // size of set
+        System.out.println("The size of set is :" +set.size());
+
+        // printing the set.
+        System.out.println("Normally prining set :"+set);
+
+        //iterator
+        Iterator it =set.iterator();
+        System.out.print("Printing using iterator.");
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+
+        // to delete all elements in the array.
+        set.clear();
     }
 }
